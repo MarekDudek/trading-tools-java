49fff369bc6fb2b5cf1346efef217379a1531835
Marek Dudek <marek.dudek@gmail.com>
2019-03-28 23:09:22 +0100

    main executable + dependencies

---
+++ src/main/java/com/marekdudek/trading_tools/Main.java
@@ -0,0 +1,11 @@
+package com.marekdudek.trading_tools;
+
+import static java.lang.System.out;
+
+public final class Main
+{
+    public static void main(final String[] args)
+    {
+        out.println("Trading tools");
+    }
+}
